0ab12e72f5806161365bd36834eefd615c6eedaa
Sean Grove <s@bushi.do>
2012-11-16 22:52:27 -0800

    Add in loader gif

--- src/jikken/views/welcome.clj
+++ src/jikken/views/welcome.clj
@@ -8,12 +8,10 @@
 (defpage "/" []
          (common/layout
            [:div.query
-            [:h1 "実験"]
-            [:h2 "Pre-built rules:"]
-            [:ul
-             (map #(vector :li (str (first (first %)))) jida/rules)]
+            [:h1 "実験 - Explore Clojure Projects"]
             [:textarea#query-text
              {:rows 3
               :placeholder "Your query"}]
             [:input#query-submit.btn.btn-large.btn-primary {:value "run" :type "submit"}]
+            [:img#loader {:style "display:none;" :src "https://www.zenboxapp.com/assets/loading.gif"}]
             [:p#results]]))
