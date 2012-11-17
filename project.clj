ceda4aa36751a531febc850511516419bbe20e36
Sean Grove <s@bushi.do>
2012-11-16 17:44:01 -0800

    Add datomic-free jar to project

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject jikken "0.1.0-SNAPSHOT"
-            :plugins [[lein-cljsbuild "0.2.9"]]
+            :plugins [[lein-cljsbuild "0.2.9"]
+                      [lein-swank "1.4.4"]]
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [domina "1.0.1"]
                            [fetch "0.1.0-alpha2"]
                            [crate "0.2.1"]
-                           [noir "1.3.0-beta3"]]
+                           [noir "1.3.0-beta3"]
+                           [com.datomic/datomic-free "0.8.3599"]]
             :min-lein-version "2.0.0"
             :repl-options {:nrepl-middleware
                            [cemerick.piggieback/wrap-cljs-repl]}
