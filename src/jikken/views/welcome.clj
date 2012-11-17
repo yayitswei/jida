77db6e82f00b8fe5a61a3d788eb46e1908de396d
Wei Hsu <wei@airtime.com>
2012-11-16 22:52:05 -0800

    design for available repos

--- src/jikken/views/welcome.clj
+++ src/jikken/views/welcome.clj
@@ -12,6 +12,12 @@
             [:h2 "Pre-built rules:"]
             [:ul
              (map #(vector :li (str (first (first %)))) jida/rules)]
+            [:div.repos.well
+             [:p "Available repos: "
+              [:a {:href "#"} "codox/codox.leiningen"]
+              ", "
+              [:a {:href "#"} "ragtime/ragtime.lein 0.3.2"]
+              ]]
             [:textarea#query-text
              {:rows 3
               :placeholder "Your query"}]
