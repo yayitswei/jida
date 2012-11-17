42593fa04598063841b2931237997cb33387d77d
Sean Grove <s@bushi.do>
2012-11-16 18:30:05 -0800

    Show a list of rules

--- src/jikken/views/welcome.clj
+++ src/jikken/views/welcome.clj
@@ -1,11 +1,15 @@
 (ns jikken.views.welcome
   (:require [jikken.views.common :as common]
+            [jikken.datomic :as jida]
             [noir.content.getting-started])
   (:use [noir.core :only [defpage]]))
+
 
 (defpage "/" []
          (common/layout
            [:p "Welcome to jikken"]
+           [:ul
+            (map #(vector :li (str (first (first %)))) jida/rules)]
            [:textarea#query-text
             {:rows 3} "the query"]
            [:input#query-submit.btn {:value "run" :type "submit"}]
