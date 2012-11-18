762b4b84f6f8b684e436910f04470c3806b4549e
Wei Hsu <wei@airtime.com>
2012-11-17 17:27:35 -0800

    trying :optimizations :simple in production

--- project.clj
+++ project.clj
@@ -26,6 +26,6 @@
                           :compiler
                           {:output-to "resources/public/js/bin/main.js"
                            :output-dir "resources/public/js/bin"
-                           :optimizations :advanced}}}
+                           :optimizations :simple}}}
                         }
             :main jikken.server)
