d0d9eabc51eef908c756bc4cfa01a9445708b43a
Sean Grove <s@bushi.do>
2012-11-30 15:34:18 -0800

    Add in frontend for saving queries

--- src/cljs/main.cljs
+++ src/cljs/main.cljs
@@ -5,6 +5,7 @@
             [domina :as d]
             [domina.events :as evt]
             [fetch.remotes :as remotes]
+            [goog.Uri :as uri]
             [goog.dom.selection :as selection])
 
   (:use-macros [crate.def-macros :only [defpartial]])
@@ -14,6 +15,12 @@
   (= document/domain "localhost"))
 
 (def host (str "http://" document/domain))
+
+(def parsed-uri
+  (goog.Uri. (-> (.-location js/window) (.-href) )))
+
+(def initial-query-id
+  (.getParameterValue parsed-uri "query-id"))
 
 (defpartial item [fields]
             [:div.result-item
@@ -41,6 +48,9 @@
   (selection/setStart text-area offset)
   (selection/setEnd text-area (inc offset)))
 
+(defn display-error [message]
+  (helper/show (d/by-id "error-messages"))
+  (d/set-html! (d/by-id "error-messages") message))
 
 (defn submit-query [_]
   (let [query (d/value (d/by-id "query-text"))
@@ -58,10 +68,6 @@
         (select-character (d/by-id "query-text") (first error-offsets))
         (helper/hide (d/by-id "loader"))))))
 
-(defn display-error [message]
-  (helper/show (d/by-id "error-messages"))
-  (d/set-html! (d/by-id "error-messages") message))
-
 (defn queue-import [_]
   (let [url (d/value (d/by-id "repo-address"))]
     (if (helper/valid-git-url? url)
@@ -71,9 +77,27 @@
         (fm/remote
          (queue-import url) [_]
          (d/set-text! (d/by-id "import-status")
-                      (str "Importing " url ".. you may not see it right away."))))
+                      (str "Importing " url "... you may not see it right away."))))
       ; Invalid url
       (display-error "That looks like an invalid Git url. It must start with 'https://'"))))
+
+(defn set-query-field [query]
+  (d/set-text! (d/by-id "query-text") query))
+
+(defn check-save-button []
+  (d/log "Checking the save button")
+  (let [query-node (d/by-id "query-text")
+        query-value (d/value query-node)]
+    (if (= (count query-value) 0)
+      (d/set-attr! (d/by-id "query-save") :disabled true)
+      (d/remove-attr! (d/by-id "query-save") :disabled))))
+
+(defn save-query! []
+  (let [title (js/prompt "Query title")
+        description (js/prompt "Describe the query")
+        query-value (d/value (d/by-id "query-text"))]
+    (letrem [result (save-query {:query query-value :description description :title title})]
+            (d/log "Save result: " result))))
 
 (defn ^:export setup []
   (fm/remote
@@ -81,7 +105,15 @@
    (d/set-html! (d/by-id "available-repos")
                 (repos result)))
   (evt/listen! (d/by-id "query-submit") :click submit-query)
+  (evt/listen! (d/by-id "query-save") :click save-query!)
   (evt/listen! (d/by-id "import-repo-btn") :click queue-import)
+  (evt/listen! (d/by-id "query-text") :keypress check-save-button)
+  (evt/listen! (d/by-id "query-text") :focus check-save-button)
+  (evt/listen! (d/by-id "query-text") :blur check-save-button)
+  (when initial-query-id
+    ;(letrem [query (get-query initial-query-id)]           (set-query-field query))
+    (set-query-field "This is an initial query!")
+    )
   (when (development?)
     (repl/connect (str host ":9000/repl"))))
 
