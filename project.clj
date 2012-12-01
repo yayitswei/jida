82109df98aafe0e8f0b66a6939931a0311904129
Sean Grove <s@bushi.do>
2012-12-01 12:40:43 -0800

    Update the version of Datomic "0.8.3599" => "0.8.3627" for @jonase

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                            [fetch "0.1.0-alpha2"]
                            [crate "0.2.1"]
                            [noir "1.3.0-beta3"]
-                           [com.datomic/datomic-free "0.8.3599"]
+                           [com.datomic/datomic-free "0.8.3627"]
                            [org.clojure/data.json "0.2.1"]
                            [com.novemberain/monger "1.4.0"]
                            [clj-redis "0.0.12"]]
